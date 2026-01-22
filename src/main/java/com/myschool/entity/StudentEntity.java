ce6271860b7489322a02efc744de270804a7da63
Manesh Edirisinghe <your-email@example.com>
2026-01-22 14:43:18 +0530

    first commit

---
+++ src/main/java/com/myschool/entity/StudentEntity.java
@@ -0,0 +1,39 @@
+package com.myschool.entity;
+
+import com.myschool.util.Gender;
+import com.myschool.util.StudentStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "students")
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer studentId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user; // Student login info
+
+    @ManyToOne
+    @JoinColumn(name = "school_id")
+    private SchoolEntity school;
+
+    private String admissionNumber;
+    private LocalDate admissionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "current_class_id")
+    private ClassEntity currentClass;
+
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private StudentStatus status;
+}
---
+++ src/main/java/com/myschool/entity/TeacherEntity.java
@@ -0,0 +1,24 @@
+package com.myschool.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "teacher")
+public class TeacherEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer schoolId;
+    private String name;
+    private String gender; // Enum වෙනුවට String භාවිතා කළා
+    private String phone;
+    private String address;
+    private String nic;
+    private String department;
+}
