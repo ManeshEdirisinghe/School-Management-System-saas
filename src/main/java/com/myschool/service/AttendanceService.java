6cd48cb04908a2d38568fe7242ee88d1e8f5badd
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-22 15:58:36 +0530

    Define AttendanceService interface for attendance management functionality

--- src/main/java/com/myschool/service/AttendanceService.java
+++ src/main/java/com/myschool/service/AttendanceService.java
@@ -1,4 +1,11 @@
 package com.myschool.service;
 
-public class AttendanceService {
+import com.myschool.dto.StudentAttendanceDto;
+import java.util.List;
+import java.time.LocalDate;
+
+public interface AttendanceService {
+    void markAttendance(StudentAttendanceDto dto);
+    List<StudentAttendanceDto> getStudentAttendance(Integer studentId);
+    List<StudentAttendanceDto> getClassAttendanceByDate(Integer classId, LocalDate date);
 }
