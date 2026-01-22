d1f8fa061227c9eca2bfbdcca88371b95cfcc021
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-22 15:27:19 +0530

    Implement ExamService and ExamServiceImpl with methods for creating exams and managing marks

--- src/main/java/com/myschool/service/ExamService.java
+++ src/main/java/com/myschool/service/ExamService.java
@@ -1,4 +1,14 @@
 package com.myschool.service;
 
-public class ExamService {
+import com.myschool.dto.ExamDto;
+import com.myschool.dto.MarkDto;
+import java.util.List;
+
+public interface ExamService {
+    void createExam(ExamDto examDto);
+    List<ExamDto> getAllExams();
+
+    void addMarks(MarkDto markDto);
+    List<MarkDto> getMarksByStudent(Integer studentId);
+    List<MarkDto> getMarksByExam(Integer examId);
 }
