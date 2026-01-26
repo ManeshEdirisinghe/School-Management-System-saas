1bf493f49d426b37e8cb9d340fd72adf76e1a40b
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-26 12:20:00 +0530

    Implement TimeSlotRepository and TimetableRepository interfaces for entity management

--- src/main/java/com/myschool/repository/TimeSlotRepository.java
+++ src/main/java/com/myschool/repository/TimeSlotRepository.java
@@ -1,4 +1,7 @@
 package com.myschool.repository;
 
-public class TimeSlotRepository {
+import com.myschool.entity.TimeSlotEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeSlotRepository extends JpaRepository<TimeSlotEntity, Integer> {
 }
