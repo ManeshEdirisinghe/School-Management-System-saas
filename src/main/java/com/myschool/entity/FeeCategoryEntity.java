c8d159dfcb5cda2c82533960596d5112bf8fb0df
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-26 17:50:21 +0530

    Add FeeCategoryEntity class with fields for fee category representation

--- src/main/java/com/myschool/entity/FeeCategoryEntity.java
+++ src/main/java/com/myschool/entity/FeeCategoryEntity.java
@@ -1,4 +1,19 @@
 package com.myschool.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "fee_categories")
 public class FeeCategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer feeCategoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id")
+    private SchoolEntity school;
+
+    private String name; // e.g., "Term Fee", "Facility Fee"
 }
