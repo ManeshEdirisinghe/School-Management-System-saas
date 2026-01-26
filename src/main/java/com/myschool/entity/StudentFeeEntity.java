531e16fcdade35d6885b229c6a6b8f0b7316c17e
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-26 17:51:41 +0530

    Add FeeStructureEntity and StudentFeeEntity classes for fee structure and student fee representation

--- src/main/java/com/myschool/entity/StudentFeeEntity.java
+++ src/main/java/com/myschool/entity/StudentFeeEntity.java
@@ -1,4 +1,29 @@
 package com.myschool.entity;
 
+import com.myschool.util.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "student_fees")
 public class StudentFeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer studentFeeId;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private StudentEntity student;
+
+    @ManyToOne
+    @JoinColumn(name = "fee_structure_id")
+    private FeeStructureEntity feeStructure;
+
+    private LocalDate dueDate;
+    private Double amountPaid; // දැනට ගෙවා ඇති මුදල
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
 }
