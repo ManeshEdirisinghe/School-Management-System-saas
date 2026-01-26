b19804f53ea51204aa0666536bb1623b9c2186da
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-26 17:54:48 +0530

    Add DTO classes for FeeCategory, FeeStructure, and StudentFee with Lombok annotations

--- src/main/java/com/myschool/dto/FeeCategoryDto.java
+++ src/main/java/com/myschool/dto/FeeCategoryDto.java
@@ -1,4 +1,9 @@
 package com.myschool.dto;
+import lombok.Data;
 
+@Data
 public class FeeCategoryDto {
+    private Integer feeCategoryId;
+    private Integer schoolId;
+    private String name;
 }
