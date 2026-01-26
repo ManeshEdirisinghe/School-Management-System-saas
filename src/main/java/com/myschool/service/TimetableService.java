146383f843fe02ae26cdc9b7c23dec0294fb2d68
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-26 13:14:52 +0530

    Implement Timetable management functionality in controller and service layers

--- src/main/java/com/myschool/service/TimetableService.java
+++ src/main/java/com/myschool/service/TimetableService.java
@@ -1,4 +1,15 @@
 package com.myschool.service;
 
-public class TimetableService {
+import com.myschool.dto.ClassSubjectDto;
+import com.myschool.dto.TimeSlotDto;
+import com.myschool.dto.TimetableDto;
+import java.util.List;
+
+public interface TimetableService {
+    void assignSubjectToClass(ClassSubjectDto dto);
+    void createTimeSlot(TimeSlotDto dto);
+    void createTimetableEntry(TimetableDto dto);
+
+    List<TimetableDto> getTimetableByClass(Integer classId);
+    List<ClassSubjectDto> getSubjectsByClass(Integer classId);
 }
