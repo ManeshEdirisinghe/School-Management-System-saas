9d104aacc927c68be9477ed333123f60bc270616
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-28 20:41:02 +0530

    Implement Teacher Attendance feature with DTO, Entity, Repository, Service, and ServiceImpl classes

--- src/main/java/com/myschool/repository/TeacherAttendanceRepository.java
+++ src/main/java/com/myschool/repository/TeacherAttendanceRepository.java
@@ -1,4 +1,13 @@
 package com.myschool.repository;
 
-public class TeacherAttendanceRepository {
+import com.myschool.entity.TeacherAttendanceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.time.LocalDate;
+
+public interface TeacherAttendanceRepository extends JpaRepository<TeacherAttendanceEntity, Integer> {
+
+    List<TeacherAttendanceEntity> findByTeacherTeacherId(Integer teacherId);
+
+    List<TeacherAttendanceEntity> findByDate(LocalDate date);
 }
