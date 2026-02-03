4dfe474aa23415ae148626c7e500ffa018a5a901
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-02-03 23:10:33 +0530

    Add DTO classes for user authentication with fields for login request and response

--- src/main/java/com/myschool/dto/LoginRequestDto.java
+++ src/main/java/com/myschool/dto/LoginRequestDto.java
@@ -1,4 +1,8 @@
 package com.myschool.dto;
+import lombok.Data;
 
+@Data
 public class LoginRequestDto {
+    private String username;
+    private String password;
 }
--- src/main/java/com/myschool/dto/LoginResponseDto.java
+++ src/main/java/com/myschool/dto/LoginResponseDto.java
@@ -1,4 +1,11 @@
 package com.myschool.dto;
+import lombok.Data;
 
+@Data
 public class LoginResponseDto {
+    private Integer userId;
+    private String username;
+    private String role;
+    private Integer specificId;
+    private String name;
 }
