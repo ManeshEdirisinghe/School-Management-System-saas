5c175d3d3138f13a29343f0dba06bab6354fd028
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-02-03 23:15:59 +0530

    Refactor UserEntity by removing unused fields and imports

--- src/main/java/com/myschool/entity/UserEntity.java
+++ src/main/java/com/myschool/entity/UserEntity.java
@@ -1,9 +1,7 @@
 package com.myschool.entity;
 
-import com.myschool.util.GeneralStatus;
 import jakarta.persistence.*;
 import lombok.Data;
-import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -13,22 +11,12 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
 
-    @ManyToOne
-    @JoinColumn(name = "school_id")
-    private SchoolEntity school;
+    @Column(unique = true)
+    private String username;
+
+    private String password; 
 
     @ManyToOne
     @JoinColumn(name = "role_id")
     private RoleEntity role;
-
-    private String username;
-    private String passwordHash;
-    private String email;
-    private String phone;
-
-    @Enumerated(EnumType.STRING)
-    private GeneralStatus status;
-
-    private LocalDateTime lastLogin;
-    private LocalDateTime createdAt;
 }
